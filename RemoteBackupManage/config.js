d6223acc051926aadac41ef66d5ec5c1d8a11bfe
bird <1069208936@qq.com>
2017-08-02 11:36:49 +0800

    config has been changed

--- RemoteBackupManage/config.js
+++ RemoteBackupManage/config.js
@@ -4,12 +4,12 @@
 
 /*配置服务器环境*/
 var databaseConfig = {
-    host:'localhost',
-    user:'root',
-    password:'123456',
+    host:'',
+    user:'',
+    password:'',
     dumpPath:'./public/download',                    //服务器端文件操作目录，必须保证有这个文件夹且为空
-    database:'posapi-prod',
-    tokenText:'RWEQAER'
+    database:'',
+    tokenText:''
 };
 
 
--- config.js
+++ config.js
@@ -2,13 +2,13 @@
  * Created by bird on 2017/8/1.
  */
 var config = {
-    host:'localhost',                                    //数据库主机名称
-    port:'3306',                                          //数据库端口号
-    user:'root',                                          //数据库用户名
-    password:'123456',                                   //数据库密码
-    manage_database:'posapi-prod',                     //想要执行导入的数据库名称
-    forCreate_database:'mysql',                         //该数据库只要存在即可
-    baseUrlPath:'http://localhost:3000/service/',    //服务器地址
+    host:'',                                    //数据库主机名称
+    port:'',                                          //数据库端口号
+    user:'',                                          //数据库用户名
+    password:'',                                   //数据库密码
+    manage_database:'',                     //想要执行导入的数据库名称
+    forCreate_database:'',                         //该数据库只要存在即可
+    baseUrlPath:'',    //服务器地址
     baseZipName:'sql.7z'                                 //压缩文件名称，可以不用修改
 };
 module.exports = config;
